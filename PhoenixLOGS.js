300192b04c7679c979aa8f07453af28601565adf
Phoenix <theemaraldblock@gmail.com>
2018-08-16 22:53:49 +0100

    Update PhoenixLOGS.js

--- PhoenixLOGS.js
+++ PhoenixLOGS.js
@@ -17,7 +17,7 @@ const ErrorTypes = {
 PixLogs.on('ready', Phoenix => {
 PixLogs.channels.get("479756702892425226").send(`${ErrorTypes.successPrefix} System Has Started Up Successfully`);
 PixLogs.channels.get("479756702892425226").send(`${ErrorTypes.infoPrefix} System Is Running On Version Phoenix API & Logs ${PixLogsVer}`);
-}
+});
 
 PixLogs.on('disconnected', Phoenix => {
 PixEmergencyLogs.send(`${ErrorTypes.errorPrefix} Phoenix API & Logs Has Been Disconnected From the Discord API <@270600034540716032> <@292684036965662722>`) 
